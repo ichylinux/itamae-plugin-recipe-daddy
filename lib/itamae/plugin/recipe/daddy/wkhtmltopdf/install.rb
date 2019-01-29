7cb6da683c8fa67f20950f73b2907868f68d7175
ichy <ichylinux@gmail.com>
2019-01-29 23:32:02 +0900

    wkhtmltopdf

---
+++ lib/itamae/plugin/recipe/daddy/wkhtmltopdf/install.rb
@@ -0,0 +1,22 @@
+require 'daddy/itamae'
+
+version = ENV['WKHTMLTOPDF_VERSION'] || WKHTMLTOPDF_VERSION
+
+case os_version
+when /rhel-6\.(.*?)/
+  execute "download wkhtmltopdf-#{version}" do
+    cwd '/var/daddy/tmp'
+    command <<-EOF
+      wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox-#{version}.centos6.x86_64.rpm
+    EOF
+    not_if "sha256sum -c #{::File.join(::File.dirname(__FILE__), "wkhtmltox-#{version}_sha256sum.txt")}"
+  end
+when /rhel-7\.(.*?)/
+  execute "download wkhtmltopdf-#{version}" do
+    cwd '/var/daddy/tmp'
+    command <<-EOF
+      wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox-#{version}.centos7.x86_64.rpm
+    EOF
+    not_if "sha256sum -c #{::File.join(::File.dirname(__FILE__), "wkhtmltox-#{version}_sha256sum.txt")}"
+  end
+end
