ed76172931fafb3377859d4138321065079e2646
ichy <ichylinux@gmail.com>
2019-01-30 00:19:39 +0900

    wkhtmltopdf

--- lib/itamae/plugin/recipe/daddy/wkhtmltopdf/install.rb
+++ lib/itamae/plugin/recipe/daddy/wkhtmltopdf/install.rb
@@ -4,20 +4,37 @@ version = ENV['WKHTMLTOPDF_VERSION'] || ItamaePluginRecipeDaddy::WKHTMLTOPDF_VER
 
 case os_version
 when /rhel-6\.(.*?)/
-  execute "download wkhtmltopdf-#{version}" do
+  execute "download wkhtmltox-#{version}" do
     cwd '/var/daddy/tmp'
     command <<-EOF
-      wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox-#{version}.centos6.x86_64.rpm
+      rm -f wkhtmltox-#{version}.centos6.x86_64.rpm
+      wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/#{version.split('-').first}/wkhtmltox-#{version}.centos6.x86_64.rpm
     EOF
-    not_if "sha256sum -c #{::File.join(::File.dirname(__FILE__), "wkhtmltox-#{version}_sha256sum.txt")}"
+    not_if "sha256sum -c #{::File.join(::File.dirname(__FILE__), "wkhtmltox-#{version}.centos6_sha256sum.txt")}"
+  end
+  
+  execute "install wkhtmltox-#{version}" do
+    cwd '/var/daddy/tmp'
+    user 'root'
+    command <<-EOF
+      yum install -y wkhtmltox-#{version}.centos6.x86_64.rpm
+    EOF
   end
 when /rhel-7\.(.*?)/
-  execute "download wkhtmltopdf-#{version}" do
+  execute "download wkhtmltox-#{version}" do
     cwd '/var/daddy/tmp'
     command <<-EOF
       rm -f wkhtmltox-#{version}.centos7.x86_64.rpm
-      wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox-#{version}.centos7.x86_64.rpm
+      wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/#{version.split('-').first}/wkhtmltox-#{version}.centos7.x86_64.rpm
     EOF
-    not_if "sha256sum -c #{::File.join(::File.dirname(__FILE__), "wkhtmltox-#{version}_sha256sum.txt")}"
+    not_if "sha256sum -c #{::File.join(::File.dirname(__FILE__), "wkhtmltox-#{version}.centos7_sha256sum.txt")}"
+  end
+  
+  execute "install wkhtmltox-#{version}" do
+    cwd '/var/daddy/tmp'
+    user 'root'
+    command <<-EOF
+      yum install -y wkhtmltox-#{version}.centos7.x86_64.rpm
+    EOF
   end
 end
