21221f1f623cf3609f6415f5875dd2926ed71f73
ichy <ichylinux@gmail.com>
2019-01-29 23:39:46 +0900

    wkhtmltopdf

--- lib/itamae/plugin/recipe/daddy/wkhtmltopdf/install.rb
+++ lib/itamae/plugin/recipe/daddy/wkhtmltopdf/install.rb
@@ -1,6 +1,6 @@
 require 'daddy/itamae'
 
-version = ENV['WKHTMLTOPDF_VERSION'] || WKHTMLTOPDF_VERSION
+version = ENV['WKHTMLTOPDF_VERSION'] || ItamaePluginRecipeDaddy::WKHTMLTOPDF_VERSION
 
 case os_version
 when /rhel-6\.(.*?)/
@@ -15,6 +15,7 @@ when /rhel-7\.(.*?)/
   execute "download wkhtmltopdf-#{version}" do
     cwd '/var/daddy/tmp'
     command <<-EOF
+      rm -f wkhtmltox-#{version}.centos7.x86_64.rpm
       wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox-#{version}.centos7.x86_64.rpm
     EOF
     not_if "sha256sum -c #{::File.join(::File.dirname(__FILE__), "wkhtmltox-#{version}_sha256sum.txt")}"
