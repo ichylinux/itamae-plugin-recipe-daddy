45eb8ea02e2b58b53e2a8d9d25c6cac81f197f1c
ichy <ichylinux@gmail.com>
2025-11-01 02:01:55 +0900

    MySQL-8.4

--- lib/itamae/plugin/recipe/daddy/mysql/server.rb
+++ lib/itamae/plugin/recipe/daddy/mysql/server.rb
@@ -1,9 +1,24 @@
 include_recipe 'daddy::mysql::common'
 
+service 'mysqld' do
+  user 'root'
+  action [:disable, :stop]
+end
+
+directory '/var/lib/mysql84' do
+  user 'root'
+end
+
 case os_version
 when /rhel-7\.(.*?)/, /rhel-8\.(.*?)/
-  package 'mysql-community-server' do
+  template '/etc/systemd/system/mysqld.service' do
     user 'root'
+  end
+
+  execute 'systemctl daemon-reload' do
+    user 'root'
+    subscribes :run, 'template[/etc/systemd/system/mysqld.service]', :immediately
+    action :nothing
   end
 else
   raise I18n.t('itamae.errors.unsupported_os_version', os_version: os_version)
